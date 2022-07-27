e237d9cfbc79ab0b45c3f61f6ec70d1ac6da4eec
Shad-Alam <shad.19bd@gmail.com>
2022-07-27 22:28:21 +0600

    adding controller and fxml file

---
+++ Main/src/main/java/com/example/main/SudukuController.java
@@ -0,0 +1,20 @@
+package com.example.main;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SudukuController implements Initializable {
+    public TextField tf_3_7;
+    public TextField tf_4_8;
+    public TextField tf_4_7;
+    public TextField tf_4_6;
+    public TextField tf_5_8;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
