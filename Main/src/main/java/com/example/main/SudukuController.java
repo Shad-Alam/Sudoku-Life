ceed3bcc85cd6ae77d73e3a678c21aa2d8937359
Shad-Alam <shad.19bd@gmail.com>
2022-07-28 12:00:59 +0600

    collecting all grid data from user

--- Main/src/main/java/com/example/main/SudukuController.java
+++ Main/src/main/java/com/example/main/SudukuController.java
@@ -90,11 +90,124 @@ public class SudukuController implements Initializable {
     public TextField tf_0_5;
     public TextField tf_0_3;
 
+    int[][] i = new int[10][10];
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        for(int a=0; a<9; a++){
+            for(int b=0; b<9; b++){
+                i[a][b] = 0;
+            }
+        }
+    }
 
+    private String emptyChecker(String s){
+        return s.length()==0 ? "0" : s;
+    }
+    private void source(){
+        i[0][0] = Integer.parseInt(emptyChecker(tf_0_0.getText()));
+        i[0][1] = Integer.parseInt(emptyChecker(tf_0_1.getText()));
+        i[0][2] = Integer.parseInt(emptyChecker(tf_0_2.getText()));
+        i[0][3] = Integer.parseInt(emptyChecker(tf_0_3.getText()));
+        i[0][4] = Integer.parseInt(emptyChecker(tf_0_4.getText()));
+        i[0][5] = Integer.parseInt(emptyChecker(tf_0_5.getText()));
+        i[0][6] = Integer.parseInt(emptyChecker(tf_0_6.getText()));
+        i[0][7] = Integer.parseInt(emptyChecker(tf_0_7.getText()));
+        i[0][8] = Integer.parseInt(emptyChecker(tf_0_8.getText()));
+
+        i[1][0] = Integer.parseInt(emptyChecker(tf_1_0.getText()));
+        i[1][1] = Integer.parseInt(emptyChecker(tf_1_1.getText()));
+        i[1][2] = Integer.parseInt(emptyChecker(tf_1_2.getText()));
+        i[1][3] = Integer.parseInt(emptyChecker(tf_1_3.getText()));
+        i[1][4] = Integer.parseInt(emptyChecker(tf_1_4.getText()));
+        i[1][5] = Integer.parseInt(emptyChecker(tf_1_5.getText()));
+        i[1][6] = Integer.parseInt(emptyChecker(tf_1_6.getText()));
+        i[1][7] = Integer.parseInt(emptyChecker(tf_1_7.getText()));
+        i[1][8] = Integer.parseInt(emptyChecker(tf_1_8.getText()));
+
+        i[2][0] = Integer.parseInt(emptyChecker(tf_2_0.getText()));
+        i[2][1] = Integer.parseInt(emptyChecker(tf_2_1.getText()));
+        i[2][2] = Integer.parseInt(emptyChecker(tf_2_2.getText()));
+        i[2][3] = Integer.parseInt(emptyChecker(tf_2_3.getText()));
+        i[2][4] = Integer.parseInt(emptyChecker(tf_2_4.getText()));
+        i[2][5] = Integer.parseInt(emptyChecker(tf_2_5.getText()));
+        i[2][6] = Integer.parseInt(emptyChecker(tf_2_6.getText()));
+        i[2][7] = Integer.parseInt(emptyChecker(tf_2_7.getText()));
+        i[2][8] = Integer.parseInt(emptyChecker(tf_2_8.getText()));
+
+        i[3][0] = Integer.parseInt(emptyChecker(tf_3_0.getText()));
+        i[3][1] = Integer.parseInt(emptyChecker(tf_3_1.getText()));
+        i[3][2] = Integer.parseInt(emptyChecker(tf_3_2.getText()));
+        i[3][3] = Integer.parseInt(emptyChecker(tf_3_3.getText()));
+        i[3][4] = Integer.parseInt(emptyChecker(tf_3_4.getText()));
+        i[3][5] = Integer.parseInt(emptyChecker(tf_3_5.getText()));
+        i[3][6] = Integer.parseInt(emptyChecker(tf_3_6.getText()));
+        i[3][7] = Integer.parseInt(emptyChecker(tf_3_7.getText()));
+        i[3][8] = Integer.parseInt(emptyChecker(tf_3_8.getText()));
+
+        i[4][0] = Integer.parseInt(emptyChecker(tf_4_0.getText()));
+        i[4][1] = Integer.parseInt(emptyChecker(tf_4_1.getText()));
+        i[4][2] = Integer.parseInt(emptyChecker(tf_4_2.getText()));
+        i[4][3] = Integer.parseInt(emptyChecker(tf_4_3.getText()));
+        i[4][4] = Integer.parseInt(emptyChecker(tf_4_4.getText()));
+        i[4][5] = Integer.parseInt(emptyChecker(tf_4_5.getText()));
+        i[4][6] = Integer.parseInt(emptyChecker(tf_4_6.getText()));
+        i[4][7] = Integer.parseInt(emptyChecker(tf_4_7.getText()));
+        i[4][8] = Integer.parseInt(emptyChecker(tf_4_8.getText()));
+
+        i[5][0] = Integer.parseInt(emptyChecker(tf_5_0.getText()));
+        i[5][1] = Integer.parseInt(emptyChecker(tf_5_1.getText()));
+        i[5][2] = Integer.parseInt(emptyChecker(tf_5_2.getText()));
+        i[5][3] = Integer.parseInt(emptyChecker(tf_5_3.getText()));
+        i[5][4] = Integer.parseInt(emptyChecker(tf_5_4.getText()));
+        i[5][5] = Integer.parseInt(emptyChecker(tf_5_5.getText()));
+        i[5][6] = Integer.parseInt(emptyChecker(tf_5_6.getText()));
+        i[5][7] = Integer.parseInt(emptyChecker(tf_5_7.getText()));
+        i[5][8] = Integer.parseInt(emptyChecker(tf_5_8.getText()));
+
+        i[6][0] = Integer.parseInt(emptyChecker(tf_6_0.getText()));
+        i[6][1] = Integer.parseInt(emptyChecker(tf_6_1.getText()));
+        i[6][2] = Integer.parseInt(emptyChecker(tf_6_2.getText()));
+        i[6][3] = Integer.parseInt(emptyChecker(tf_6_3.getText()));
+        i[6][4] = Integer.parseInt(emptyChecker(tf_6_4.getText()));
+        i[6][5] = Integer.parseInt(emptyChecker(tf_6_5.getText()));
+        i[6][6] = Integer.parseInt(emptyChecker(tf_6_6.getText()));
+        i[6][7] = Integer.parseInt(emptyChecker(tf_6_7.getText()));
+        i[6][8] = Integer.parseInt(emptyChecker(tf_6_8.getText()));
+
+        i[7][0] = Integer.parseInt(emptyChecker(tf_7_0.getText()));
+        i[7][1] = Integer.parseInt(emptyChecker(tf_7_1.getText()));
+        i[7][2] = Integer.parseInt(emptyChecker(tf_7_2.getText()));
+        i[7][3] = Integer.parseInt(emptyChecker(tf_7_3.getText()));
+        i[7][4] = Integer.parseInt(emptyChecker(tf_7_4.getText()));
+        i[7][5] = Integer.parseInt(emptyChecker(tf_7_5.getText()));
+        i[7][6] = Integer.parseInt(emptyChecker(tf_7_6.getText()));
+        i[7][7] = Integer.parseInt(emptyChecker(tf_7_7.getText()));
+        i[7][8] = Integer.parseInt(emptyChecker(tf_7_8.getText()));
+
+        i[8][0] = Integer.parseInt(emptyChecker(tf_8_0.getText()));
+        i[8][1] = Integer.parseInt(emptyChecker(tf_8_1.getText()));
+        i[8][2] = Integer.parseInt(emptyChecker(tf_8_2.getText()));
+        i[8][3] = Integer.parseInt(emptyChecker(tf_8_3.getText()));
+        i[8][4] = Integer.parseInt(emptyChecker(tf_8_4.getText()));
+        i[8][5] = Integer.parseInt(emptyChecker(tf_8_5.getText()));
+        i[8][6] = Integer.parseInt(emptyChecker(tf_8_6.getText()));
+        i[8][7] = Integer.parseInt(emptyChecker(tf_8_7.getText()));
+        i[8][8] = Integer.parseInt(emptyChecker(tf_8_8.getText()));
+
+
+//        for(int c=0; c<9; c++) {
+//            System.out.println("i[4][" + c + "] = Integer.parseInt(emptyChecker(tf_4_" + c + ".getText()));");
+//        }
     }
 
     public void newGame(ActionEvent actionEvent) {
+        source();
+
+        for(int a=1; a<9; a++){
+            for(int b=0; b<9; b++){
+                System.out.println(i[a][b]);
+            }
+        }
     }
 }
