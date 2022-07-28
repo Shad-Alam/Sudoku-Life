404ddfb6900af3eae57f5886ea7e1076c8472355
Shad-Alam <shad.19bd@gmail.com>
2022-07-28 11:16:13 +0600

    connect suduku fxml with SudukuController

--- Main/src/main/java/com/example/main/SudukuController.java
+++ Main/src/main/java/com/example/main/SudukuController.java
@@ -1,5 +1,6 @@
 package com.example.main;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
 
@@ -12,9 +13,88 @@ public class SudukuController implements Initializable {
     public TextField tf_4_7;
     public TextField tf_4_6;
     public TextField tf_5_8;
+    public TextField tf_5_7;
+    public TextField tf_5_6;
+    public TextField tf_3_8;
+    public TextField tf_3_6;
+    public TextField tf_3_1;
+    public TextField tf_4_2;
+    public TextField tf_4_1;
+    public TextField tf_4_0;
+    public TextField tf_5_2;
+    public TextField tf_5_1;
+    public TextField tf_5_0;
+    public TextField tf_3_2;
+    public TextField tf_3_0;
+    public TextField tf_3_4;
+    public TextField tf_4_5;
+    public TextField tf_4_4;
+    public TextField tf_4_3;
+    public TextField tf_5_5;
+    public TextField tf_5_4;
+    public TextField tf_5_3;
+    public TextField tf_3_5;
+    public TextField tf_3_3;
+    public TextField tf_6_7;
+    public TextField tf_7_8;
+    public TextField tf_7_7;
+    public TextField tf_7_6;
+    public TextField tf_8_8;
+    public TextField tf_8_7;
+    public TextField tf_8_6;
+    public TextField tf_6_8;
+    public TextField tf_6_6;
+    public TextField tf_6_4;
+    public TextField tf_7_5;
+    public TextField tf_7_4;
+    public TextField tf_7_3;
+    public TextField tf_8_5;
+    public TextField tf_8_4;
+    public TextField tf_8_3;
+    public TextField tf_6_5;
+    public TextField tf_6_3;
+    public TextField tf_6_1;
+    public TextField tf_7_2;
+    public TextField tf_7_1;
+    public TextField tf_7_0;
+    public TextField tf_8_2;
+    public TextField tf_8_1;
+    public TextField tf_8_0;
+    public TextField tf_6_2;
+    public TextField tf_6_0;
+    public TextField tf_0_1;
+    public TextField tf_1_2;
+    public TextField tf_1_1;
+    public TextField tf_1_0;
+    public TextField tf_2_2;
+    public TextField tf_2_1;
+    public TextField tf_2_0;
+    public TextField tf_0_2;
+    public TextField tf_0_0;
+    public TextField tf_0_7;
+    public TextField tf_1_8;
+    public TextField tf_1_7;
+    public TextField tf_1_6;
+    public TextField tf_2_8;
+    public TextField tf_2_7;
+    public TextField tf_2_6;
+    public TextField tf_0_8;
+    public TextField tf_0_6;
+    public TextField tf_0_4;
+    public TextField tf_1_5;
+    public TextField tf_1_4;
+    public TextField tf_1_3;
+    public TextField tf_2_5;
+    public TextField tf_2_4;
+    public TextField tf_2_3;
+    public TextField tf_0_5;
+    public TextField tf_0_3;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+    }
+
+    public void newGame(ActionEvent actionEvent) {
     }
 }
