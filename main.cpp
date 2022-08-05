7731340f281dce368988f0a2c7286081ec325c19
Shad-Alam <shad.19bd@gmail.com>
2022-08-05 18:28:56 +0600

    update changes

--- main.cpp
+++ main.cpp
@@ -28,184 +28,86 @@ int main(){
 		}cout << endl;
 	}cout << endl << endl;
 	
-	int part1[10], p = 0;
-	bool visit1[10];
 	
+	// part 1
+	bool visit1[10];
 	for(int a=0; a<=2; a++){
 		for(int b=0; b<=2; b++){
-			visit1[i[a][b]] = false;
+			visit1[i[a][b]] = false;	
 		}
 	}
-	
+	// remove value in circle is found
+	int j1[10]; int p1 = 0;
 	for(int a=0; a<=2; a++){
 		for(int b=0; b<=2; b++){
 			if(visit1[i[a][b]]){
 				int ssd = i[a][b];
 				i[a][b] = 0;
 				
-				for(int c=1; c<=9; c++){
-					if(!visit1[c]){
-						i[a][b] = c;
-						
-						bool port1 = false;
-						for(int c1=b-1; c1>=0; c1--){
-							if(i[a][c1]==c){
-								port1 = true;
-								break;
-							}
-						}
-						
-						for(int c1=a-1; c1>=0; c1--){
-							if(i[c1][b]==c){
-								port1 = true;
-								break;
-							}
-						}
-						
-						if(port1==false){
-							for(int d=b+1; d<9; d++){
-								if(i[a][d]==c){
-									i[a][d] = ssd;
-									break;
-								}
-							}
-							visit1[c] = true;
-						}
-						break;
-					}
-				}
-			}
-			visit1[i[a][b]] = true;
-		}
-	}
-	
-	// 2
-	
-	p = 0;
-	bool visit2[10];
-	for(int a=3; a<=5; a++){
-		for(int b=0; b<=2; b++){
-			visit2[i[a][b]] = false;
-		}
-	}
-	
-	for(int a=3; a<=5; a++){
-		// part - 1 
-		// if any value match in circle(3*3) then swap
-		for(int b=0; b<=2; b++){
-			if(visit2[i[a][b]]){
-				int ssd = i[a][b];
-				i[a][b] = 0;
-				
-				for(int c=1; c<=9; c++){
-					if(!visit2[c]){
-						i[a][b] = c;
-						
-						bool port1 = false;
-						for(int c1=b-1; c1>=0; c1--){
-							if(i[a][c1]==c){
-								port1 = true;
-								break;
-							}
-						}
-						
-						for(int c1=a-1; c1>=0; c1--){
-							if(i[c1][b]==c){
-								port1 = true;
-								break;
-							}
-						}
-						
-						if(port1==false){
-							for(int d=b+1; d<9; d++){
-								if(i[a][d]==c){
-									i[a][d] = ssd;
-									break;
-								}
-							}
-							visit2[c] = true;
-						}
-						break;
-					}
-				}
-			}
-			visit2[i[a][b]] = true;
-		}
-	}
-	
-	int memo[10], p1 = 0;
-	for(int a=3; a<=5; a++){
-		// part-2
-		// in a straight line, if any value match then current value will be zero and store match value 
-		for(int b=0; b<=2; b++){
-			int ssd = i[a][b];
-			bool port1 = false;
-			for(int c1=b-1; c1>=0; c1--){
-				if(i[a][c1]==ssd){
-					port1 = true;
-					break;
-				}
-			}
-						
-			for(int c1=a-1; c1>=0; c1--){
-				if(i[c1][b]==ssd){
-					port1 = true;
-					break;
-				}
-			}
-			
-			if(port1){
-				memo[p1] = ssd;
+				j1[p1] = ssd;
 				p1++;
-				cout << ssd << endl;
-				i[a][b] = 0;
+				
+			}visit1[i[a][b]] = true;
+		}
+	}
+	
+	// add all necessary value
+	p1 = 0;
+	// 1 6 7 9
+	for(int c=1; c<=n; c++){
+		if(!visit1[c]){
+			for(int a=0; a<=2; a++){
+				bool port1 = false;
+				for(int b=0; b<=2; b++){
+					if(i[a][b]==0){
+						int ssd = j1[p1]; // 4, 8, 5 8
+						for(int d=b+1; d<9; d++){
+							if(i[a][d]==c){
+								i[a][b] = c;
+								i[a][d] = ssd;
+								p1++;
+								visit1[c] = true;
+								port1 = true;
+								break;
+							}
+						}
+						
+						if(port1){
+							break;
+						}
+					}
+				}
+				
+				if(port1){
+					break;
+				}
 			}
 		}
 	}
-		// part - 3
-		// pass all storing value to original position
-	sort(memo,memo+p1);
-	bool visiting[10];
-	for(int c=0; c<p1; c++){
-		cout << memo[c] << " ";
-		visiting[memo[c]] = false;
-	}cout << endl << endl;
 	
-	
-	
-	for(int a=3; a<=5; a++){
+	// swap value if match
+	for(int a=0; a<=2; a++){
+		bool visiting[10];
+		for(int b=1; b<=9; b++){
+			visiting[b] = false;
+		}
 		for(int b=0; b<=2; b++){
-			if(i[a][b]==0){
-				int ssd = i[a][b];
-				
-				for(int c=0; c<p1; c++){
-					if(!visiting[memo[c]]){						
-						bool port1 = false;
-						for(int c1=b-1; c1>=0; c1--){
-							if(i[a][c1]==memo[c]){
-								port1 = true;
-								break;
-							}
+			visiting[i[b][a]] = true;
+		}
+		for(int b=0; b<=2; b++){
+			int ssd = i[b][a];
+			visit[a][b] = true;
+			for(int c=3; c<9; c++){
+				if(i[c][a]==ssd){
+					i[c][a] = 0;
+					for(int d=0; d<9; d++){
+						if(visit[c][d]){
+							continue;
 						}
-						
-						for(int c1=a-1; c1>=0; c1--){
-							if(i[c1][b]==memo[c]){
-								port1 = true;
-								break;
-							}
-						}
-						
-						cout << "port" << endl;
-						for(int c1=0; c1<9; c1++){
-							if(i[a][c1]==memo[c]){
-								port1 = true;
-								break;
-							}
-						}cout << endl << endl;
-						
-						if(port1==false){
-							i[a][b] = memo[c];
-							visiting[memo[c]] = true;
+						if(!visiting[i[c][d]] and d!=a){
+							i[c][a] = i[c][d];
+							i[c][d] = ssd;
+							visiting[i[c][d]] = true;
 							break;
 						}
 					}
@@ -213,6 +115,9 @@ int main(){
 			}
 		}
 	}
+	
+	
+
 
 	
 	for(int a=0; a<n; a++){
