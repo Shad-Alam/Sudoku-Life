1c1de25502069040430cbc34054745590c3aa37d
Shad-Alam <shad.19bd@gmail.com>
2023-01-13 15:36:50 +0600

    adding Level Controller

--- Main/src/main/java/com/example/main/LevelController.java
+++ Main/src/main/java/com/example/main/LevelController.java
@@ -1,2 +1,29 @@
-package com.example.main;public class LevelController {
+package com.example.main;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LevelController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        
+    }
+
+    public void btn_easy(ActionEvent actionEvent) {
+    }
+
+    public void btn_medium(ActionEvent actionEvent) {
+    }
+
+    public void btn_hard(ActionEvent actionEvent) {
+    }
+
+    public void btn_expert(ActionEvent actionEvent) {
+    }
+
+    public void btn_evil(ActionEvent actionEvent) {
+    }
 }
